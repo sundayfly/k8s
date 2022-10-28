7eab470231268b179bd19ef8ea6836ba64dc9148
siupang <shaopeng1995@gmail.com>
2022-10-28 22:23:17 +0800

    Time 2022年10月28日 星期五 22时23分17秒 CST

--- deploy.sh
+++ deploy.sh
@@ -11,14 +11,15 @@ echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
+msg="Time `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
 
 # Push source and build repos.
-git push origin master
+#git push origin master
+git push
 
 # Come Back up to the Project Root
-#cd ..
+##cd ..
